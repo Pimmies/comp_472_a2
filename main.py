65d8d744bbf7b4e8f9273a03a259582ebf24cd97
Julie <julie.pierrisnard@gmail.com>
2021-06-15 23:54:04 -0400

    generates training and testing sets with positive and negative reviews. Non-tokenized

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ from requests import get
 from bs4 import BeautifulSoup
 import re
 import csv
+import os
+
+#############################################################################################
+#1.1 CSV
 
 #containers
 csv_fields = ["Episode Name", "Season", "Review Link", "Year"]
@@ -36,3 +40,58 @@ with open(csv_fileName, 'w') as csvfile:
     csv_writer = csv.writer(csvfile) # creating a csv writer object 
     csv_writer.writerow(csv_fields)  
     csv_writer.writerows(csv_rows)
+
+
+###########################################################################################
+#1.1 BUILDING A TRAINING SET AND TESTING SET
+#makes:
+#a training set with positive and negative reviews
+#a testing set with positive and negative reviews
+#not yet tokenized
+
+training_path = "./training_set"
+testing_path = "./testing_set"
+try:
+    os.mkdir(training_path)
+    os.mkdir(testing_path)
+except OSError:
+    print ("Creation of the directory failed") #will fail if directory already exists
+else:
+    print ("Successfully created the directory")
+
+pos_training = True
+neg_training = True
+training_pos_number = 0
+training_neg_number = 0
+for episode in csv_rows:
+	response = get(episode[2] + '?spoiler=hide&sort=helpfulnessScore&dir=desc&ratingFilter=0')  #get the episode review link and filter out the spoilers
+	html_soup = BeautifulSoup(response.text, 'html.parser') #creates beautifulsoup object from URL
+	review_container = html_soup.find_all('div', class_ = "review-container") #contains all reviews on the first page of the episode
+
+	for review in review_container:
+		if(review.span.span is not None): #only taking reviews that have a number rating
+			if(int(review.span.span.text) < 8): #negative review
+				if(neg_training):
+					training_neg_number += 1 
+					f = open("./training_set/training_negative.txt", "a", encoding='utf-8')
+				else:
+					f = open("./testing_set/testing_negative.txt", "a", encoding='utf-8')
+				f.write(review.a.text) #review title
+				f.write(review.find('div', class_ = "text show-more__control").text + "\n") #review text
+				f.write('/\n')
+				f.close()
+				neg_training =  not neg_training
+			else: #positive review
+				if(pos_training):
+					training_pos_number += 1
+					f = open("./training_set/training_positive.txt", "a", encoding='utf-8')
+				else:
+					f = open("./testing_set/testing_positive.txt", "a", encoding='utf-8')
+				f.write(review.a.text) #review title
+				f.write(review.find('div', class_ = "text show-more__control").text + "\n")
+				f.write('/\n')
+				f.close()
+				pos_training = not pos_training
+
+print("Amount of positive reviews in the training set is: ", training_pos_number)
+print("Amount of negative reviews in the training set is: ", training_neg_number)
